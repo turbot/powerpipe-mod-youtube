972404e964ad65ca7a2242182775ac56a78b5c9f
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2024-03-04 20:33:56 +0530

    Update mod.pp

--- mod.pp
+++ mod.pp
@@ -3,5 +3,5 @@ mod "youtube" {
   description   = "Explore comprehensive insights into YouTube channel performance, including subscriber growth, video engagement, and earnings analysis, through interactive visualizations crafted with PostgreSQL queries via Powerpipe."
   color         = "#FF0000"
   documentation = file("./README.md")
-  categories    = ["postgres"]
+  categories    = ["dashboard", "postgres"]
 }
