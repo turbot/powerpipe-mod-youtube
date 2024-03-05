afde911b92447a4f1f1a0ec09f13e7c947303e93
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2024-03-05 21:28:58 +0530

    Update mod.pp

--- mod.pp
+++ mod.pp
@@ -5,3 +5,9 @@ mod "youtube" {
   documentation = file("./README.md")
   categories    = ["dashboard", "postgres"]
 }
+
+opengraph {
+  title       = "Powerpipe Mod for YouTube"
+  description = "Explore comprehensive insights into YouTube channel performance, including subscriber growth, video engagement, and earnings analysis, through interactive visualizations crafted with PostgreSQL queries via Powerpipe."
+  image       = "/images/mods/turbot/youtube-social-graphic.png"
+}
